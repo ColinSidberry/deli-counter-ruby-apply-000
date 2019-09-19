dd6d7248a4daa9b04468cfa66d8a6aaf541e2a9f
Colin Sidberry <colinsidberry@gmail.com>
2019-09-19 21:27:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,16 +1,16 @@
 # katzDeliLine = []
 
-def take_a_number(line,name)
-  line << name
+def take_a_number(line,number)
+  line << number
   count = line.length.to_s
 
-  puts "Welcome, " + line[-1] + ". You are number " + count + " in line."
+  puts "You are # " + number.to_s + "."
 end
 
 #________________________________
 
 def line (deli_line)
-  if deli_line.length == 0 or if deli_line.length == nil
+  if deli_line.length == 0# or if deli_line.length == nil
     puts "The line is currently empty."
   end
 
@@ -35,7 +35,7 @@ end
 #________________________________
 
 def now_serving(line_arr)
-  if line_arr.length == 0 or line_arr.length == nil 
+  if line_arr.length == 0 #or line_arr.length == nil 
     puts "There is nobody waiting to be served!"
   end
   current = line_arr.shift
